5456871af142c09f9a7c2848b885ab250c77b744
Fabio Eduardo Murarolli <fabio.murarolli@dextra-sw.com>
2021-01-06 14:44:41 -0300

    Adicionando detalhes do contato;

--- app/src/main/java/com/examplo/contatos/contatos/detalhes/DetalhesDoContatoFragment.kt
+++ app/src/main/java/com/examplo/contatos/contatos/detalhes/DetalhesDoContatoFragment.kt
@@ -1,14 +1,14 @@
 package com.examplo.contatos.contatos.detalhes
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.examplo.contatos.CONTATO_EXTRA_ID
 import com.examplo.contatos.Contato
 import com.examplo.contatos.R
+import kotlinx.android.synthetic.main.fragment_detalhes_do_contato.*
 
-class DetalhesDoContatoFragment: Fragment(R.layout.fragment_detalhes_do_contato) {
+class DetalhesDoContatoFragment : Fragment(R.layout.fragment_detalhes_do_contato) {
 
     private val contatoDetalhes by lazy {
         requireArguments().getSerializable(CONTATO_EXTRA_ID) as Contato
@@ -16,8 +16,21 @@ class DetalhesDoContatoFragment: Fragment(R.layout.fragment_detalhes_do_contato)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.i("Contatos", "Chegou contato: " + contatoDetalhes.nome)
+        configurarFotoDoContato()
+        configurarNomeDoContato()
+        configurarTelefoneDoContato()
     }
 
+    private fun configurarFotoDoContato() {
+        image_view_foto_contato.setImageResource(contatoDetalhes.foto)
+    }
+
+    private fun configurarNomeDoContato() {
+        text_view_nome_contato.text = contatoDetalhes.nome
+    }
+
+    private fun configurarTelefoneDoContato() {
+        text_view_numero_contato.text = contatoDetalhes.telefone
+    }
 
 }
